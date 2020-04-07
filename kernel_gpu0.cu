08ca8350ebb38072e03c32b8e525586af421b969
Fatima <fatimahojeij97@gmail.com>
2020-04-07 21:34:15 +0300

    checking if error from kernel DRASTIC CHANGES

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -21,49 +21,38 @@ __global__ void spmspm(COOMatrix *result){
 
 void sparseNN(Vector* result, COOMatrix* outBuffer, COOMatrix** layerWeights, float bias, unsigned int numLayers) {
 
-        Timer timer;
-
-        // Convert featureVectors to CSR
-        startTime(&timer);
-
-        // Allocate GPU memory
-        startTime(&timer);
-
-
-        //outBuffer_d allocation
-        COOMatrix *outBuffer_d;
-		unsigned int* out_rowIdxs_d;
-		unsigned int* out_colIdxs_d;
-		float* out_values_d;
-		cudaMalloc((void**)&outBuffer_d, sizeof(COOMatrix));
-		cudaMalloc((void**)&out_rowIdxs_d, outBuffer->capacity * sizeof(unsigned int));
-		cudaMalloc((void**)&out_colIdxs_d, outBuffer->capacity * sizeof(unsigned int));
-		cudaMalloc((void**)&out_values_d, outBuffer->capacity * sizeof(float));
+    //outBuffer_d allocation
+    COOMatrix *outBuffer_d;
+	unsigned int* out_rowIdxs_d;
+	unsigned int* out_colIdxs_d;
+	float* out_values_d;
+	cudaMalloc((void**)&outBuffer_d, sizeof(COOMatrix));
+	cudaMalloc((void**)&out_rowIdxs_d, outBuffer->capacity * sizeof(unsigned int));
+	cudaMalloc((void**)&out_colIdxs_d, outBuffer->capacity * sizeof(unsigned int));
+	cudaMalloc((void**)&out_values_d, outBuffer->capacity * sizeof(float));
         
-        printElapsedTime(timer, "Allocate outBuffer");
         
  
 	//copying outbuffer
-    cudaMemcpy(outBuffer_d, outBuffer, sizeof(COOMatrix), cudaMemcpyHostToDevice);
+	cudaMemcpy(outBuffer_d, outBuffer, sizeof(COOMatrix), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_rowIdxs_d, outBuffer->rowIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
-    cudaMemcpy(out_colIdxs_d, outBuffer->colIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
-    cudaMemcpy(out_values_d, outBuffer->values, outBuffer->capacity * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(out_colIdxs_d, outBuffer->colIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaMemcpy(out_values_d, outBuffer->values, outBuffer->capacity * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(&(outBuffer_d->rowIdxs), &out_rowIdxs_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
 	cudaMemcpy(&(outBuffer_d->colIdxs), &out_colIdxs_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
 	cudaMemcpy(&(outBuffer_d->values), &out_values_d, sizeof(float*), cudaMemcpyHostToDevice);
-        cudaDeviceSynchronize();
-        stopTime(&timer);
-        printElapsedTime(timer, "Allocation time on GPU Memory");
+    cudaDeviceSynchronize();
 
-		spmspm <<<1, 1>>> (outBuffer_d);
+	
+	spmspm <<<1, 1>>> (outBuffer_d);
 		
 				
                 
-				cudaMemcpy(outBuffer->rowIdxs, outBuffer_d->rowIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-				cudaMemcpy(outBuffer->colIdxs, outBuffer_d->colIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-				cudaMemcpy(outBuffer->values, outBuffer_d->values, outBuffer->capacity * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(outBuffer->rowIdxs, outBuffer_d->rowIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(outBuffer->colIdxs, outBuffer_d->colIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(outBuffer->values, outBuffer_d->values, outBuffer->capacity * sizeof(float), cudaMemcpyDeviceToHost);
 		
-				printf("%f \n", outBuffer->values[0]);
+	printf("%f \n", outBuffer->values[0]);
 
 
 }
