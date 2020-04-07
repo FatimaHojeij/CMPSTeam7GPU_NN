3b258c04013e3ae84ec7a713008e2349d2fd2298
Fatima <fatimahojeij97@gmail.com>
2020-04-07 23:36:02 +0300

    checking if error from kernel DRASTIC CHANGES

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -7,11 +7,6 @@
 #include "timer.h"
 
 
-#define THRESHOLD 0.000001
-#define YMAX 32
-#define threads 512
-
-
 __global__ void spmspm(COOMatrix *result){ 
 	
 	result->rowIdxs[0] = 1;
@@ -22,7 +17,7 @@ __global__ void spmspm(COOMatrix *result){
 void sparseNN(Vector* result, COOMatrix* outBuffer, COOMatrix** layerWeights, float bias, unsigned int numLayers) {
 
     //outBuffer_d allocation
-    COOMatrix *outBuffer_d;
+	COOMatrix *outBuffer_d;
 	unsigned int* out_rowIdxs_d;
 	unsigned int* out_colIdxs_d;
 	float* out_values_d;
@@ -30,34 +25,30 @@ void sparseNN(Vector* result, COOMatrix* outBuffer, COOMatrix** layerWeights, fl
 	cudaMalloc((void**)&out_rowIdxs_d, outBuffer->capacity * sizeof(unsigned int));
 	cudaMalloc((void**)&out_colIdxs_d, outBuffer->capacity * sizeof(unsigned int));
 	cudaMalloc((void**)&out_values_d, outBuffer->capacity * sizeof(float));
-        
-        
- 
+
+
+
 	//copying outbuffer
-	//cudaMemcpy(outBuffer_d, outBuffer, sizeof(COOMatrix), cudaMemcpyHostToDevice);
+	cudaMemcpy(outBuffer_d, outBuffer, sizeof(COOMatrix), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_rowIdxs_d, outBuffer->rowIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_colIdxs_d, outBuffer->colIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_values_d, outBuffer->values, outBuffer->capacity * sizeof(float), cudaMemcpyHostToDevice);
-	outBuffer->rowIdxs = out_rowIdxs_d;
-	outBuffer->colIdxs = out_colIdxs_d;
-	outBuffer->values = out_values_d;
-	cudaMemcpy(outBuffer_d, outBuffer, sizeof(COOMatrix), cudaMemcpyHostToDevice);
-	
-    cudaDeviceSynchronize();
+	cudaMemcpy(&(outBuffer_d->rowIdxs), &out_rowIdxs_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
+	cudaMemcpy(&(outBuffer_d->colIdxs), &out_colIdxs_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
+	cudaMemcpy(&(outBuffer_d->values), &out_values_d, sizeof(float*), cudaMemcpyHostToDevice);
+	cudaDeviceSynchronize();
+
 
 	spmspm <<<1, 1>>> (outBuffer_d);
-		
-	cudaDeviceSynchronize();			
-           
-	cudaMemcpy(outBuffer, outBuffer_d, sizeof(COOMatrix), cudaMemcpyDeviceToHost);
+
+
+	//copy back       
 	cudaMemcpy(outBuffer->rowIdxs, out_rowIdxs_d, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	cudaMemcpy(outBuffer->colIdxs, out_colIdxs_d, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	cudaMemcpy(outBuffer->values, out_values_d, outBuffer->capacity * sizeof(float), cudaMemcpyDeviceToHost);
-	
-	// 9. Point to host pointer in host struct.
-    //h_a->arr = h_arr;
-		
+
 	printf("%f \n", outBuffer->values[0]);
+
 
 
 }
