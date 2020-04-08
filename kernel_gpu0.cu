5041698a55359dfa0199293bcffdca57584bf210
FatimaHojeij <fatimahojeij97@gmail.com>
2020-04-09 01:33:08 +0300

    Update kernel_gpu0.cu

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -21,14 +21,14 @@ void sparseNN(Vector* result, COOMatrix* outBuffer, COOMatrix** layerWeights, fl
 	unsigned int* out_rowIdxs_d;
 	unsigned int* out_colIdxs_d;
 	float* out_values_d;
-	//unsigned int* out_nnz_d;
+	unsigned int* out_nnz_d;
 	//unsigned int* out_nnz_h = &(outBuffer->nnz);//(unsigned int*) malloc(sizeof(unsigned int*));
 	//*out_nnz_h = outBuffer->nnz;
 	cudaMalloc((void**)&outBuffer_d, sizeof(COOMatrix));
 	cudaMalloc((void**)&out_rowIdxs_d, outBuffer->capacity * sizeof(unsigned int));
 	cudaMalloc((void**)&out_colIdxs_d, outBuffer->capacity * sizeof(unsigned int));
 	cudaMalloc((void**)&out_values_d, outBuffer->capacity * sizeof(float));
-	//cudaMalloc((void**)&out_nnz_d, sizeof(unsigned int));
+	cudaMalloc((void**)&out_nnz_d, sizeof(unsigned int));
 
 
 
@@ -37,13 +37,13 @@ void sparseNN(Vector* result, COOMatrix* outBuffer, COOMatrix** layerWeights, fl
 	cudaMemcpy(out_rowIdxs_d, outBuffer->rowIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_colIdxs_d, outBuffer->colIdxs, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemcpy(out_values_d, outBuffer->values, outBuffer->capacity * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(outBuffer_d->nnz, outBuffer->nnz, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaMemcpy(out_nnz_d, outBuffer->nnz, sizeof(unsigned int), cudaMemcpyHostToDevice);
 	//cudaMemcpy(out_nnz_d, out_nnz_h, sizeof(unsigned int), cudaMemcpyHostToDevice);
 	//cudaMemcpy(out_nnz_d, &(outBuffer->nnz), sizeof(unsigned int), cudaMemcpyHostToDevice);
 	cudaMemcpy(&(outBuffer_d->rowIdxs), &out_rowIdxs_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
 	cudaMemcpy(&(outBuffer_d->colIdxs), &out_colIdxs_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
 	cudaMemcpy(&(outBuffer_d->values), &out_values_d, sizeof(float*), cudaMemcpyHostToDevice);
-	//cudaMemcpy(&(outBuffer_d->nnz), &out_nnz_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
+	cudaMemcpy(&(outBuffer_d->nnz), &out_nnz_d, sizeof(unsigned int*), cudaMemcpyHostToDevice);
 	cudaDeviceSynchronize();
 	printf("nnz before kernel call %d \n", outBuffer->nnz);
 
@@ -54,10 +54,11 @@ void sparseNN(Vector* result, COOMatrix* outBuffer, COOMatrix** layerWeights, fl
 	cudaMemcpy(outBuffer->rowIdxs, out_rowIdxs_d, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	cudaMemcpy(outBuffer->colIdxs, out_colIdxs_d, outBuffer->capacity * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	cudaMemcpy(outBuffer->values, out_values_d, outBuffer->capacity * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaMemcpy(outBuffer->nnz, outBuffer_d->nnz, sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaMemcpy(outBuffer->nnz, out_nnz_d, sizeof(unsigned int), cudaMemcpyHostToDevice);
 	//cudaMemcpy(out_nnz_h, out_nnz_d, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 	//outBuffer->nnz = *out_nnz_h;
 	cudaDeviceSynchronize();
+	//outBuffer->nnz = out_nnz_d;
 	printf("%f \n", outBuffer->values[0]);
 	printf("nnz after kernel call %d \n", outBuffer->nnz);
 
