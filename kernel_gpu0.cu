d9f8dc334652adf3d9222e5e0ad3c5fa00017793
FatimaHojeij <fatimahojeij97@gmail.com>
2020-04-09 18:12:39 +0300

    Update kernel_gpu0.cu

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -9,8 +9,8 @@ __global__ void spmspm(COOMatrix *result, unsigned int* nnz_out, CSRMatrix A, CS
 	
 	result->rowIdxs[0] = 1;
 	result->colIdxs[0] = 1;
-	result->values[0] = A.values[3];
-	*nnz_out = B.values[0];
+	result->values[0] = = B.values[0];
+	*nnz_out = A.values[0];
 }
 
 COOMatrix* createEmptyCOO(unsigned int numRows, unsigned int numCols, unsigned int capacity) {
