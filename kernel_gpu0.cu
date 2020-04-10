9eabb2149783c1bc9c12c9a80aee0e429e549db5
FatimaHojeij <fatimahojeij97@gmail.com>
2020-04-10 22:30:28 +0300

    Update kernel_gpu0.cu

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -20,20 +20,20 @@ __global__ void spmspm(COOMatrix *result, unsigned int* nnz_out, CSRMatrix A, CS
 			unsigned int colPtrB = B.colPtrs[c];
 			unsigned int nnzB = B.colPtrs[c + 1] - colPtrB;
 			if(nnzB>0) {
-// 				unsigned int* rowIdxsB = B.rowIdxs + colPtrB;
-//                                 float* valueB = B.values + colPtrB;
-//                                 float sum = 0.0f;
-//                                 unsigned int ia = 0, ib = 0;
-// 				while(ia < nnzA && ib < nnzB) { 
-// 					unsigned int colIdx = colIdxsA[ia];
-//                                         unsigned int rowIdx = rowIdxsB[ib];
-// 					if(colIdx < rowIdx) {
-//                                                 ia++;
-//                                         } else if(colIdx > rowIdx) {
-//                                                 ib++;
-//                                         }
-// 					++ia;
-// 					++ib;
+				unsigned int* rowIdxsB = B.rowIdxs + colPtrB;
+                                float* valueB = B.values + colPtrB;
+                                float sum = 0.0f;
+                                unsigned int ia = 0, ib = 0;
+				while(ia < nnzA && ib < nnzB) { 
+					unsigned int colIdx = colIdxsA[ia];
+                                        unsigned int rowIdx = rowIdxsB[ib];
+					if(colIdx < rowIdx) {
+                                                ia++;
+                                        } else if(colIdx > rowIdx) {
+                                                ib++;
+                                        }
+					++ia;
+					++ib;
 				}
 				*nnz_out = rowPtrA; //r;
 				result->values[0] = colPtrB;// c;
