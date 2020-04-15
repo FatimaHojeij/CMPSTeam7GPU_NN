280a5f3dc15ab40db3d3be8fc7fa867ab8b4c57c
Safaa Dyab <33197552+safaad@users.noreply.github.com>
2020-04-15 13:27:34 +0300

    Update kernel_gpu0.cu

--- kernel_gpu0.cu
+++ kernel_gpu0.cu
@@ -272,15 +272,16 @@ __global__ void convertFromCOOToCSR_kernel(unsigned int* inrowIdxs, unsigned int
 		unsigned int rowPtrA = rowPtrs[row];
 		unsigned int nnzA = rowPtrs[row + 1] - rowPtrs[row];
 
-		for (unsigned int j = 0; j < nnzA; ++j) {
+		// for (unsigned int j = 0; j < nnzA; ++j) {
 
-			if (atomicCAS(&colIdxs[j + rowPtrA], UINT_MAX, col) == UINT_MAX) {
-				values[j + rowPtrA] = val;
-				break;
-			}
+		// 	if (atomicCAS(&colIdxs[j + rowPtrA], UINT_MAX, col) == UINT_MAX) {
+		// 		values[j + rowPtrA] = val;
+		// 		break;
+		// 	}
 
-		}
-
+		// }
+			colIdxs[i] =1;
+			values[i]=1.0f;
 
 
 	}
