66031bfdf5549806fb56a6840d86c67339f21a58
Safaa Dyab <33197552+safaad@users.noreply.github.com>
2020-04-27 15:42:10 +0300

    Update kernel_gpu6.cu

--- kernel_gpu6.cu
+++ kernel_gpu6.cu
@@ -1,8 +1,3 @@
-/*#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
-#include <stdlib.h>*/
-
 
 #include <stdio.h>
 
@@ -16,14 +11,11 @@
 #define threads 32
 #define BLOCK_DIM 1024
 #define CAPACITY 25498020
-#define COARSE_FACTOR 2
-#define WARP_SIZE 32
+
 
 __global__ void spmspm(COOMatrix *result, CSRMatrix A, CSCMatrix B, float bias, unsigned int* nnz_out) {
-
+	unsigned int r = blockIdx.y*blockDim.y + threadIdx.y;
 	unsigned int c = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int segmenty = COARSE_FACTOR*blockDim.y*blockIdx.y;
-    unsigned int row = segmenty + threadIdx.y;
 
     __shared__ unsigned int rowIdxs_s [threads*threads];
     __shared__ unsigned int colIdxs_s [threads*threads];
@@ -35,86 +27,77 @@ __global__ void spmspm(COOMatrix *result, CSRMatrix A, CSCMatrix B, float bias, 
     }
     __syncthreads();
 
-    for(unsigned int cr= 0 ; cr<COARSE_FACTOR;++cr){
-        unsigned int r = row + cr*threads;
+	if (r < A.numRows && c < B.numCols) {
+		unsigned int rowPtrA = A.rowPtrs[r];
+		unsigned int nnzA = A.rowPtrs[r + 1] - rowPtrA;
 
-        if (r < A.numRows && c < B.numCols) {
-            unsigned int rowPtrA = A.rowPtrs[r];
-            unsigned int nnzA = A.rowPtrs[r + 1] - rowPtrA;
+		unsigned int colPtrB = B.colPtrs[c];
+		unsigned int nnzB = B.colPtrs[c + 1] - colPtrB;
+		if (nnzA > 0 && nnzB > 0) { // if a row is not all zeros , we do computation otherwise we skip row
 
-            unsigned int colPtrB = B.colPtrs[c];
-            unsigned int nnzB = B.colPtrs[c + 1] - colPtrB;
-            if (nnzA > 0 && nnzB > 0) { // if a row is not all zeros , we do computation otherwise we skip row
-                float sum = 0.0f;
-                unsigned int ia = 0, ib = 0;
-                while (ia < nnzA && ib < nnzB) { // loops over all non zeros from A and B and stop when there is no more non zero
+			float sum = 0.0f;
+			unsigned int ia = 0, ib = 0;
+			while (ia < nnzA && ib < nnzB) { // loops over all non zeros from A and B and stop when there is no more non zero
 
-                    unsigned int colIdx = A.colIdxs[rowPtrA + ia]; //single item col index from A
-                    unsigned int rowIdx = B.rowIdxs[colPtrB + ib]; //single item row index from B
-                    if (rowIdx < B.nnz && colIdx < A.nnz) {
-                        if (colIdx < rowIdx) {
-                            ia++;
-                        }
-                        else if (colIdx > rowIdx) {
-                            ib++;
-                        }
-                        else {
-                            sum += A.values[rowPtrA + ia] * B.values[ib + colPtrB];// do the multiplication of the row that matches the column
-                            ia++;
-                            ib++;
-                        }
+				unsigned int colIdx = A.colIdxs[rowPtrA + ia]; //single item col index from A
+				unsigned int rowIdx = B.rowIdxs[colPtrB + ib]; //single item row index from B
+				if (rowIdx < B.nnz && colIdx < A.nnz) {
+					if (colIdx < rowIdx) {
+						ia++;
+					}
+					else if (colIdx > rowIdx) {
+						ib++;
+					}
+					else {
+						sum += A.values[rowPtrA + ia] * B.values[ib + colPtrB];// do the multiplication of the row that matches the column
+						ia++;
+						ib++;
+					}
+				}
+
+			}
+			if (sum > THRESHOLD || sum < -THRESHOLD) { //if not smaller than abs(threshold)
+				sum += bias; //add to it the bias
+				//Remove negative and zero values
+				if (sum > 0) {//if end result is positive otherwise I also do not want to add it to result
+					if (sum > YMAX) { //make sure it is on an upper limit
+						sum = YMAX;
                     }
+                    
+                    unsigned int nnzIndxTemp = atomicAdd(&nnz_s, 1); //counts how many non zero elements I have
+                    if(nnzIndxTemp < threads*threads){
+					    rowIdxs_s[nnzIndxTemp] = r;
+					    colIdxs_s[nnzIndxTemp] = c;
+                        values_s[nnzIndxTemp] = sum;
+                    }else{
+                        nnzIndxTemp = atomicAdd(nnz_out, 1);
+					    result->rowIdxs[nnzIndxTemp] = r;
+					    result->colIdxs[nnzIndxTemp] = c;
+                        result->values[nnzIndxTemp] = sum;
 
-                }
-                if (sum > THRESHOLD || sum < -THRESHOLD) { //if not smaller than abs(threshold)
-                    sum += bias; //add to it the bias
-                    //Remove negative and zero values
-                    if (sum > 0) {//if end result is positive otherwise I also do not want to add it to result
-                        if (sum > YMAX) { //make sure it is on an upper limit
-                            sum = YMAX;
-                        }
- 
-                        if(nnz_s < threads*threads){
-							unsigned int nnzIndxTemp = atomicAdd(&nnz_s, 1); //counts how many non zero elements I have
-                            rowIdxs_s[nnzIndxTemp] = r;
-                            colIdxs_s[nnzIndxTemp] = c;
-                            values_s[nnzIndxTemp] = sum;
-                        }else{
-                            unsigned int nnzIndxTemp = atomicAdd(nnz_out, 1);
-                            result->rowIdxs[nnzIndxTemp] = r;
-                            result->colIdxs[nnzIndxTemp] = c;
-                            result->values[nnzIndxTemp] = sum;
-    
-                        }
                     }
-                }
+				}
+			}
 
 
-            }
-        }
+		}
     }
-
+    
     __syncthreads();
 
-    unsigned int tid = threadIdx.x*blockDim.x + threadIdx.y; 
-    if(tid < threads*threads && tid < nnz_s){
-
- 
-		unsigned int nnzIndxTemp = atomicAdd(nnz_out, 1);
-		result->rowIdxs[nnzIndxTemp] = rowIdxs_s[tid];
-		result->colIdxs[nnzIndxTemp] = colIdxs_s[tid];
-		result->values[nnzIndxTemp] = values_s[tid];
-
-        
+    //unsigned int tid = threadIdx.x*blockDim.x + threadIdx.y; 
+    if(threadIdx.x == 0 && threadIdx.y == 0){
+		unsigned int nnzIndxTemp = atomicAdd(nnz_out, nnz_s);
+		
+		for(unsigned int offset = 0;offset<nnz_s;++offset){
+        	result->rowIdxs[nnzIndxTemp + offset] = rowIdxs_s[offset];
+        	result->colIdxs[nnzIndxTemp + offset] = colIdxs_s[offset];
+			result->values[nnzIndxTemp + offset] = values_s[offset];
+		}
 
     }
 
-
-
-
 }
-
-
 
 
 
@@ -274,8 +257,156 @@ __global__ void Binning_kernel(unsigned int* inrowIdxs, unsigned int* incolIdxs,
 		float val = invalues[i];
 		unsigned int init = rowPtrs[row];
 		unsigned int nnzIdx = atomicAdd(&rowPtrsBin[row], 1);
-		colIdxs[nnzIdx+init] = col;
-		values[nnzIdx+init]=val;
+		colIdxs[nnzIdx + init] = col;
+		values[nnzIdx + init] = val;
+	}
+
+}
+
+
+__global__ void scan_kernel(unsigned int* input, unsigned int* output, unsigned int* partialSums, unsigned int N) {
+
+	// TODO
+
+
+	unsigned int segment = 2 * blockDim.x * blockIdx.x;
+	unsigned int i = segment + threadIdx.x;
+
+	__shared__ unsigned int input_s[2 * BLOCK_DIM];
+
+	int tid = threadIdx.x;
+
+
+	if (i < N)
+	{
+		input_s[tid] = input[i];
+	}
+	else
+	{
+		input_s[tid] = 0;
+	}
+	if (i + BLOCK_DIM < N)
+	{
+		input_s[tid + BLOCK_DIM] = input[i + BLOCK_DIM];
+	}
+	else
+	{
+		input_s[tid + BLOCK_DIM] = 0;
+	}
+	__syncthreads();
+
+
+	//reduction step
+	for (unsigned int stride = 1; stride <= BLOCK_DIM; stride *= 2)
+	{
+		int index = (threadIdx.x + 1) * 2 * stride - 1;
+		if (index < 2 * BLOCK_DIM)
+			input_s[index] += input_s[index - stride];
+		__syncthreads();
+	}
+
+	//save partial sum
+	if (threadIdx.x == 0)
+	{
+		partialSums[blockIdx.x] = input_s[2 * BLOCK_DIM - 1];
+		input_s[2 * BLOCK_DIM - 1] = 0.0f;
+
+	}
+
+	__syncthreads();
+
+	//post reduction step
+	for (unsigned int stride = BLOCK_DIM; stride > 0; stride /= 2)
+	{
+		int index = (threadIdx.x + 1) * 2 * stride - 1;
+
+		if (index < 2 * BLOCK_DIM)
+		{
+			//add then swap
+			unsigned int temp = input_s[index];
+			input_s[index] += input_s[index - stride];
+			input_s[index - stride] = temp;
+		}
+
+		__syncthreads();
+	}
+
+
+	if (i < N)
+	{
+		output[i] = input_s[tid];
+	}
+	if (i + BLOCK_DIM < N)
+	{
+		output[i + BLOCK_DIM] = input_s[tid + BLOCK_DIM];
+	}
+
+}
+
+__global__ void add_kernel(unsigned int* output, unsigned int* partialSums, unsigned int N) {
+
+	// TODO
+	unsigned int i = 2 * blockIdx.x*blockDim.x + threadIdx.x;
+	if (blockIdx.x != 0) {
+		if (i < N) {
+			output[i] += partialSums[blockIdx.x];
+		}
+		if (i + BLOCK_DIM < N) {
+			output[i + BLOCK_DIM] += partialSums[blockIdx.x];
+		}
+	}
+
+}
+//output_d rowptrs n = numrows +1
+void scan_gpu_d(unsigned int* input_d, unsigned int* output_d, unsigned int N) {
+
+	// Configurations
+	const unsigned int numThreadsPerBlock = BLOCK_DIM;
+	const unsigned int numElementsPerBlock = 2 * numThreadsPerBlock;
+	const unsigned int numBlocks = (N + numElementsPerBlock - 1) / numElementsPerBlock;
+
+	// Allocate partial sums
+
+	unsigned int *partialSums_d;
+	cudaMalloc((void**)&partialSums_d, numBlocks * sizeof(unsigned int));
+	cudaDeviceSynchronize();
+
+
+	scan_kernel << < numBlocks, numThreadsPerBlock >> > (input_d, output_d, partialSums_d, N);
+	cudaDeviceSynchronize();
+
+
+	// Scan partial sums then add
+	if (numBlocks > 1) {
+
+		// Scan partial sums
+		scan_gpu_d(partialSums_d, partialSums_d, numBlocks);
+
+		// Add scanned sums
+		add_kernel << < numBlocks, numThreadsPerBlock >> > (output_d, partialSums_d, N);
+
+	}
+
+	// Free memory
+	cudaFree(partialSums_d);
+	cudaDeviceSynchronize();
+
+}
+
+
+
+__global__ void Binning_kernel(unsigned int* inrowIdxs, unsigned int* incolIdxs, float* invalues, unsigned int* rowPtrs, unsigned int* colIdxs, float* values, unsigned int nnz, unsigned int numRows, unsigned int* rowPtrsBin) {
+
+	int i = threadIdx.x + blockIdx.x*blockDim.x;
+
+	if (i < nnz) {
+		unsigned int row = inrowIdxs[i];
+		unsigned int col = incolIdxs[i];
+		float val = invalues[i];
+		unsigned int init = rowPtrs[row];
+		unsigned int nnzIdx = atomicAdd(&rowPtrsBin[row], 1);
+		colIdxs[nnzIdx + init] = col;
+		values[nnzIdx + init] = val;
 	}
 
 
@@ -285,6 +416,7 @@ __global__ void  sorting_kernel(unsigned int* colIdxs, float* values, unsigned i
 	int i = threadIdx.x + blockIdx.x*blockDim.x;
 
 	if (i < numRows) {
+
 		unsigned int nnzA = rowPtrs[i + 1] - rowPtrs[i];
 
 		if (nnzA > 0) {
@@ -313,6 +445,7 @@ __global__ void  sorting_kernel(unsigned int* colIdxs, float* values, unsigned i
 	}
 
 }
+
 //converts from CSRMatrix to Vector and a vector of indices where the row is not all zeros
 void findNonzeroRows(Vector* v, CSRMatrix* A) {
 	unsigned int nnz = 0;
@@ -350,6 +483,7 @@ COOMatrix* createEmptyCOO(unsigned int numRows, unsigned int numCols, unsigned i
 void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeights, float bias, unsigned int numLayers) {
 
 
+	
 	Timer timer;
 
 	// Convert featureVectors to CSR
@@ -461,8 +595,8 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 	rowPtrstmp = (unsigned int *)malloc((inBuffer_d.numRows + 1) * sizeof(unsigned int));
 	cudaMalloc((void**)&rowPtrstmp_d, (inBuffer_d.numRows + 1) * sizeof(unsigned int));
 
-	for(unsigned int i=0; i<inBuffer_d.numRows+1;i++){
-	 	rowPtrstmp[i]=0;
+	for (unsigned int i = 0; i < inBuffer_d.numRows + 1;i++) {
+		rowPtrstmp[i] = 0;
 	}
 
 	cudaMemcpy(rowPtrstmp_d, rowPtrstmp, (inBuffer_d.numRows + 1) * sizeof(unsigned int), cudaMemcpyHostToDevice);
@@ -482,7 +616,7 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 
 
 		dim3 numThreadsPerBlock3(threads, threads);
-		dim3 numBlocks3((W_d[layer].numCols + numThreadsPerBlock3.x  - 1) / (numThreadsPerBlock3.x), (inBuffer_d.numRows + numThreadsPerBlock3.y*COARSE_FACTOR - 1) /( numThreadsPerBlock3.y*COARSE_FACTOR));
+		dim3 numBlocks3((W_d[layer].numCols + numThreadsPerBlock3.x - 1) / numThreadsPerBlock3.x, (inBuffer_d.numRows + numThreadsPerBlock3.y - 1) / numThreadsPerBlock3.y);
 
 		spmspm << <numBlocks3, numThreadsPerBlock3 >> > (outBuffer_d, inBuffer_d, W_d[layer], bias, out_nnz_d);
 
@@ -492,7 +626,6 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 		printf("kernel time for layer %u", layer);
 		stopTimeAndPrint(&timer, "");
 
-
 		cudaMemcpy(out_nnz_h, out_nnz_d, sizeof(unsigned int), cudaMemcpyDeviceToHost);
 		//printf("nnz %d\n", *out_nnz_h);
 
@@ -500,7 +633,8 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 		inBuffer_d.nnz = *out_nnz_h;
 		inBuffer_d.numCols = W_d[layer].numCols;
 
-		cudaDeviceSynchronize();
+
+
 
 		startTime(&timer);
 
@@ -542,12 +676,8 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 		}
 
 		cudaDeviceSynchronize();
-
-
-		// Free memory
-
-		cudaFree(partialSums_d);
-
+        cudaFree(partialSums_d);
+        
 		//Binning
 		for (unsigned int i = 0; i < inBuffer_d.numRows + 1;i++) {
 			rowPtrstmp[i] = 0;
@@ -586,11 +716,12 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 		}
 
 		cudaDeviceSynchronize();
-			
+
+
 	}
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 	// Copy data from GPU
@@ -601,9 +732,15 @@ void sparseNN(Vector* result, COOMatrix* featureVectors, COOMatrix** layerWeight
 	inBuffer->numRows = inBuffer_d.numRows;
 	inBuffer->numCols = inBuffer_d.numCols;
 	inBuffer->nnz = inBuffer_d.nnz;
-	cudaMemcpy(inBuffer->rowPtrs, inBuffer_d.rowPtrs, (inBuffer_d.numRows + 1) * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(inBuffer->colIdxs, inBuffer_d.colIdxs, inBuffer_d.nnz * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-	cudaMemcpy(inBuffer->values, inBuffer_d.values, inBuffer_d.nnz * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(rowPtrstmp, inBuffer_d.rowPtrs, sizeof(unsigned int) * (inBuffer_d.numRows + 1), cudaMemcpyDeviceToHost);
+	cudaMemcpy(outBuffer->colIdxs, inBuffer_d.colIdxs,inBuffer_d.nnz* sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(outBuffer->values, inBuffer_d.values, inBuffer_d.nnz * sizeof(float), cudaMemcpyDeviceToHost);
+
+
+
+	inBuffer->rowPtrs = rowPtrstmp;
+	inBuffer->colIdxs = outBuffer->colIdxs;
+	inBuffer->values = outBuffer->values;
 
 
 	cudaDeviceSynchronize();
